131c92eedf2e4045a09d98b5a94397f5a9441944
PouyaHakimi <hakimifard.pouya@gmail.com>
2025-05-08 17:56:58 +0200

    extend the docker container with eureka server

--- Quiz-Service-Microservices/src/main/java/com/example/demo/controller/QuizController.java
+++ Quiz-Service-Microservices/src/main/java/com/example/demo/controller/QuizController.java
@@ -29,4 +29,9 @@ public class QuizController {
         return quizService.QuizResult(id,response);
     }
 
+    @GetMapping("test")
+    public String test(){
+        return "docker is working";
+    }
+
 }
